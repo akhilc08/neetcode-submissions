2accd82f04db23c3a5c73ca6a2279c765acfb6a8
Saiakhil Chilaka <akhilchilakawork@gmail.com>
2026-04-09 00:15:04 -0400

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.py
@@ -0,0 +1,59 @@
+class WordDictionary:
+
+    class Node: 
+        def __init__(self, char):
+            self.val = char
+            self.children = {}
+
+        def add(self, node): 
+            self.children[node.val] = node
+
+
+    def __init__(self):
+        self.root = self.Node('*')
+        
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for char in word: 
+            if char in curr.children:
+                curr = curr.children[char]
+            else: 
+                child = self.Node(char)
+                curr.children[char] = child
+                curr = child
+        found = self.Node('*')
+        curr.children["*"] = found
+        
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for i,char in enumerate(word):
+            if char == '.':
+                for child in curr.children.values(): 
+                    if self.search_root(word[i+1:], child): return True
+                else: return False
+            elif char in curr.children:
+                curr = curr.children[char] 
+            else: 
+                return False
+        
+        if '*' in curr.children: 
+            return True
+        return False
+    def search_root(self, word, root):
+        curr = root
+        for i,char in enumerate(word):
+            if char == '.':
+                for child in curr.children.values(): 
+                    if self.search_root(word[i+1:], child): return True
+                else: return False
+            elif char in curr.children:
+                curr = curr.children[char] 
+            else: 
+                return False
+        
+        if '*' in curr.children: 
+            return True
+        return False
+        
