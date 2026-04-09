7cfd37dd4f78960f1333012f277f68e387bab26f
Saiakhil Chilaka <akhilchilakawork@gmail.com>
2026-04-09 00:02:49 -0400

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,57 @@
+class PrefixTree:
+
+    class Node: 
+        def __init__(self, char):
+            self.val = char
+            self.children = {}
+
+        def add(node): 
+            self.children[node.val] = node
+            
+
+    def __init__(self):
+        self.root = self.Node('*')
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word: 
+            if char in curr.children:
+                curr = curr.children[char]
+            else: 
+                child = self.Node(char)
+                curr.children[char] = child
+                curr = child
+        found = self.Node('*')
+        curr.children["*"] = found
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for char in word:
+            if char in curr.children:
+                curr = curr.children[char] 
+            else: 
+                return False
+        
+        if '*' in curr.children: 
+            return True
+        return False
+
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for char in prefix:
+            if char in curr.children:
+                curr = curr.children[char] 
+            else: 
+                return False
+        
+        def dfs(root):
+            if "*" in root.children:
+                return True
+            for child in root.children.values():
+                if dfs(child): return True
+            return False
+
+        return dfs(curr)
+
