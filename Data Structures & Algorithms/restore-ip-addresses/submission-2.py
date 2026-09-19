954ba9930945c9eee10590b44a2d05c4b949f79d
Saiakhil Chilaka <akhilchilakawork@gmail.com>
2026-09-19 16:45:58 -0400

    Add: restore-ip-addresses - submission-2

---
+++ Data Structures & Algorithms/restore-ip-addresses/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+
+        res = []
+        ip = []
+
+        def back(i,j): 
+            if i == j == len(s) and len(ip) == 4: 
+                res.append(".".join(ip))
+            
+            for k in range(j+1,len(s)+1):
+                if self.valid(s,i,k): 
+                    ip.append(s[i:k])
+                    back(k,k)
+                    ip.pop()
+
+        back(0,0)
+        return res
+
+
+
+    
+    def valid(self, s, l, r): 
+        if s[l] == "0" and l != r-1: return False
+        if int(s[l:r]) > 255: return False
+        return True
+        
