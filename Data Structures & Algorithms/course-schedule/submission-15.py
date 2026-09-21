b112773e57a0943b99f9938111678e1bae6781fd
Saiakhil Chilaka <akhilchilakawork@gmail.com>
2026-09-21 12:26:58 -0400

    Add: course-schedule - submission-15

---
+++ Data Structures & Algorithms/course-schedule/submission-15.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        reqs = defaultdict(list)
+        for c1,c2 in prerequisites: 
+            reqs[c1].append(c2)
+        
+        visited = set()
+        visiting = set()
+
+        def dfs(node): 
+            if node in visited: return True
+            if node in visiting: return False
+
+            visiting.add(node)
+            for child in reqs[node]: 
+                if not dfs(child): return False
+            
+            visiting.remove(node)
+            visited.add(node)
+            return True
+
+
+
+        for i in range(numCourses):
+            if not dfs(i): return False
+        return True
+
+
+
+        
